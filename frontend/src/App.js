bfc3d8237e8139c7463115ad70c97fa0fcaf7b51
janiapurva <jani.apurva@yahoo.com>
2021-06-22 19:45:29 -0400

    create data file to show dyanamic view of product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-
+import data from './data'
 
 function App() {
   return (
-    <div class="grid-container">
-      <header class="row">
+    <div className="grid-container">
+      <header className="row">
         <div>
-          <a class="brand" href="index.html">Amaze</a>
+          <a className="brand" href="index.html">
+            Amaze
+          </a>
         </div>
         <div>
           <a href="cart.html">Cart</a>
@@ -13,209 +15,45 @@ function App() {
         </div>
       </header>
       <main>
-        <div class="row center">
-          <div class="card">
-            <a href="product.html">
-              <img
-                class="medium"
-                src="/template/images/p1.jpeg"
-                alt="product"
-              />
-            </a>
-            <div class="card-body">
+        <div className="row center">
+          {
+            data.products.map(product => (
+              <div className="card">
               <a href="product.html">
-                <h2>AE Plain Button-Up Shirt
-                </h2>
+                <img className="medium" src="./images/p1.jpeg" alt="product" />
               </a>
-              <div class="rating">
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star-o"></i>
-                </span>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star-o"></i>
+                  </span>
+                </div>
+                <div className="price">$85</div>
               </div>
-              <div class="price">$85</div>
             </div>
-          </div>
+  
 
-          <div class="card">
-            <a href="product.html">
-              <!-- image size: 680px by 830px-->
-              <img
-                class="medium"
-                src="/template/images/p2.jpeg"
-                alt="product"
-              />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>AE Short-Sleeve Button-Up Shirt</h2>
-              </a>
-              <div class="rating">
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star-half-o"></i>
-                </span>
-              </div>
-              <div class="price">$77</div>
-            </div>
-          </div>
-
-          <div class="card">
-            <a href="product.html">
-              <img
-                class="medium"
-                src="/template/images/p3.jpeg"
-                alt="product"
-              />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>AE Pineapple Button-Up Shirt</h2>
-              </a>
-              <div class="rating">
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star-half-o"></i>
-                </span>
-              </div>
-              <div class="price">$88</div>
-            </div>
-          </div>
-
-          <div class="card">
-            <a href="product.html">
-              <img
-                class="medium"
-                src="/template/images/p4.jpeg"
-                alt="product"
-              />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>AE Hawaiian  Button-Up Shirt</h2>
-              </a>
-              <div class="rating">
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-              </div>
-              <div class="price">$45</div>
-            </div>
-          </div>
-
-          <div class="card">
-            <a href="product.html">
-              <img
-                class="medium"
-                src="/template/images/p5.jpeg"
-                alt="product"
-              />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>AE Floral Button-Up Shirt</h2>
-              </a>
-              <div class="rating">
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-              </div>
-              <div class="price">$65</div>
-            </div>
-          </div>
-          <div class="card">
-            <a href="product.html">
-              <!-- image size: 680px by 830px-->
-              <img
-                class="medium"
-                src="/template/images/p6.jpeg"
-                alt="product"
-              />
-            </a>
-            <div class="card-body">
-              <a href="product.html">
-                <h2>AE Printed Button-Up Shirt</h2>
-              </a>
-              <div class="rating">
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star"></i>
-                </span>
-                <span>
-                  <i class="fa fa-star-o"></i>
-                </span>
-              </div>
-              <div class="price">$75</div>
-            </div>
-          </div>
+            ))
+          }
+         
         </div>
       </main>
-      <footer class="row center">All right reserved</footer>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+const data = {
+  products:[
+    {
+      _id:"1",
+      name:"AE Plain Button-Up Shirt",
+      category:"Shirts",
+      image:"/frontend/public/images/p1.jpeg",
+      price:85,
+      brand:'American Eagle',
+      raing:4.5,
+      numReviews:10,
+      description: "High quality product"
+    },
+    {
+      _id:"2",
+      name:"Short-Sleeve Button-Up Shirt",
+      category:"Shirts",
+      image:"/frontend/public/images/p2.jpeg",
+      price:77,
+      brand:'Nike',
+      raing:4.5,
+      numReviews:10,
+      description: "High quality product"
+    },
+    {
+      _id:"3",
+      name:"Pineapple Button-Up Shirt",
+      category:"Shirts",
+      image:"/frontend/public/images/p3.jpeg",
+      price:88,
+      brand:'Addidas',
+      raing:4.5,
+      numReviews:10,
+      description: "High quality product"
+    },
+    {
+      _id:"4",
+      name:"Nike Slim Pant",
+      category:"Pants",
+      image:"/frontend/public/images/p4.jpeg",
+      price:95,
+      brand:'Nike',
+      raing:4.5,
+      numReviews:14,
+      description: "High quality product"
+    },
+    {
+      _id:"5",
+      name:" Express Slim Pant",
+      category:"Pants",
+      image:"/frontend/public/images/p5.jpeg",
+      price:110,
+      brand:'Express',
+      raing:4.5,
+      numReviews:10,
+      description: "High quality product"
+    },
+    {
+      _id:"6",
+      name:"Under Armour Fit Pant",
+      category:"Pants",
+      image:"/frontend/public/images/p6.jpeg",
+      price:75,
+      brand:'Under Armour',
+      raing:4.0,
+      numReviews:10,
+      description: "High quality product"
+    }
+  ]
+}
