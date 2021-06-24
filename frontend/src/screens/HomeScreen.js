07f3c8ae4f6cf8b01b3fe85f7185627ef654164e
janiapurva <jani.apurva@yahoo.com>
2021-06-24 19:44:18 -0400

    create error message components

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,40 @@
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import Product from "../components/Product";
+import LoadingBox from "../components/LodingBox";
+import MessageBox from "../components/MessageBox";
 
 function HomeScreen() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
   return (
-    <div className="row center">
-      {products.map((product) => (
-        <Product key={product._id} product={product} />
-      ))}
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox varient="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
