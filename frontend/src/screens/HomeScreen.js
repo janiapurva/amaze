fc130fbac35d9e66d194d510cb231c82c0ab9c1d
janiapurva <jani.apurva@yahoo.com>
2021-06-24 18:35:39 -0400

    install axios and make fetach request to server

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,23 @@
-import data from "../data";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import Product from "../components/Product";
 
-
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="row center">
-    {data.products.map((product) => (
-      <Product key={product._id} product={product} />
-    ))}
-  </div>
-  )
+      {products.map((product) => (
+        <Product key={product._id} product={product} />
+      ))}
+    </div>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
