0fb528206b18e270e8dbb8b15796e8886b036d24
janiapurva <jani.apurva@yahoo.com>
2021-06-24 16:05:52 -0400

    create backend folder and server

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products:[
+    {
+      _id:"1",
+      name:"AE Plain Button-Up Shirt",
+      category:"Shirts",
+      image:"/images/p1.jpeg",
+      price:85,
+      countInStock:10,
+      brand:'American Eagle',
+      rating:4.5,
+      numReviews:10,
+      description: "High quality product"
+    },
+    {
+      _id:"2",
+      name:"Short-Sleeve Button-Up Shirt",
+      category:"Shirts",
+      image:"/images/p2.jpeg",
+      price:77,
+      countInStock:20,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:10,
+      description: "High quality product"
+    },
+    {
+      _id:"3",
+      name:"Pineapple Button-Up Shirt",
+      category:"Shirts",
+      image:"/images/p3.jpeg",
+      price:88,
+      countInStock:0,
+      brand:'Addidas',
+      rating:4.5,
+      numReviews:10,
+      description: "High quality product"
+    },
+    {
+      _id:"4",
+      name:"Nike Slim Pant",
+      category:"Pants",
+      image:"/images/p4.jpeg",
+      price:95,
+      countInStock:15,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:14,
+      description: "High quality product"
+    },
+    {
+      _id:"5",
+      name:" Express Slim Pant",
+      category:"Pants",
+      image:"/images/p5.jpeg",
+      price:110,
+      countInStock:5,
+      brand:'Express',
+      rating:4.5,
+      numReviews:10,
+      description: "High quality product"
+    },
+    {
+      _id:"6",
+      name:"Under Armour Fit Pant",
+      category:"Pants",
+      image:"/images/p6.jpeg",
+      price:75,
+      countInStock:12,
+      brand:'Under Armour',
+      rating:4.0,
+      numReviews:10,
+      description: "High quality product"
+    }
+  ]
+}
+
+export default data;
