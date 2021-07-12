05ab0ed9ff6a2b9356cdfb5489e74d1beb448917
janiapurva <jani.apurva@yahoo.com>
2021-07-11 20:16:48 -0400

    create paypal buton

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,11 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amaze", {
 });
 
 app.use("/api/users", userRouter);
-
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
-
+app.get('/api/config/paypal',(req,res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+})
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
